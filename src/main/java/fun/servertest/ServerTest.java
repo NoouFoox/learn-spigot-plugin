a3447d36bc9b172cf15f8b6ef54b56426a9f6a52
贺俊浩 <nougat_hx@qq.com>
2024-04-14 22:09:23 +0800

    init

---
+++ src/main/java/fun/servertest/ServerTest.java
@@ -0,0 +1,17 @@
+package fun.servertest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ServerTest extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // 打开触发
+        System.out.println("runrunrun哈哈哈");
+    }
+
+    @Override
+    public void onDisable() {
+        // 关闭触发
+    }
+}
