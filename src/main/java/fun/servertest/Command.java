cc05ac1caf3e97fb27a7076b29be7d2d1254efe1
贺俊浩 <nougat_hx@qq.com>
2024-04-15 20:35:48 +0800

    init

--- src/main/java/fun/servertest/Command.java
+++ src/main/java/fun/servertest/Command.java
@@ -2,11 +2,14 @@ package fun.servertest;
 
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.Arrays;
 import java.util.Objects;
@@ -21,9 +24,16 @@ public class Command implements CommandExecutor {
             return false;
         }
         if (args.length == 1 && args[0].equals("open")) {
-            Inventory inventory = Bukkit.createInventory(null, 9, "我的箱子");
-            Inventory inventory2 = Bukkit.createInventory(null, InventoryType.WORKBENCH, "我的工作台");
+            Inventory inventory = Bukkit.createInventory(null, 9, "城镇列表");
+//            Inventory inventory2 = Bukkit.createInventory(null, InventoryType.WORKBENCH, "我的工作台");
+            ItemStack item = new ItemStack(Material.GLOW_BERRIES,1);
+            ItemMeta itemMeta = item.getItemMeta();
+            if (itemMeta != null) {
+                itemMeta.setDisplayName("千金镇");
+            }
+            item.setItemMeta(itemMeta);
             player.openInventory(inventory);
+            inventory.addItem(item);
         }
         if (args.length == 2 && args[0].equals("gm") && args[1].equals("1")) {
             player.setGameMode(GameMode.CREATIVE);
--- src/main/java/fun/servertest/MyListener.java
+++ src/main/java/fun/servertest/MyListener.java
@@ -1,14 +1,44 @@
 package fun.servertest;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyListener implements Listener {
     @EventHandler
-    public void playerJoin(PlayerJoinEvent event){
+    public void playerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        event.setJoinMessage("进入了服务器"+player.getName());
+        event.setJoinMessage("进入了服务器" + player.getName());
+    }
+
+    @EventHandler
+    public void click(InventoryClickEvent event) {
+        Inventory inventory = event.getInventory();
+        if (event.getView().getTitle().equals("城镇列表") && event.getRawSlot() == 0) {
+            ItemStack itemStack = new ItemStack(Material.RED_BANNER,1);
+            ItemMeta itemMeta = itemStack.getItemMeta();
+//            alt+167
+            if (itemMeta != null) {
+                itemMeta.setDisplayName("§3测试");
+                List<String> lore = new ArrayList<>();
+                lore.add("道具介绍");
+                lore.add("§1哈哈哈");
+                itemMeta.setLore(lore);
+            }
+            itemStack.setItemMeta(itemMeta);
+            Player player = (Player) event.getWhoClicked();
+            player.getInventory().addItem(itemStack);
+//            取消默认事件
+            event.setCancelled(true);
+        }
     }
 }
